91d9b765a04e3bacac901964aac5a976849a1b79
hatrunghai19 <87486706+hatrunghai19@users.noreply.github.com>
2021-08-06 14:26:29 +0700

    Update nlar.js

--- Javascript/nlar.js
+++ Javascript/nlar.js
@@ -30,7 +30,7 @@ app.controller('myController', function($scope) {
 	$scope.smsphone = "sms:0398810162";
 	$scope.email = "mailto:hatrunghai19@gmail.com";
 	$scope.title2 = "My Story";
-	$scope.contentStory1 = "KO là biệt danh đầu tiên. Khi còn là cậu học sinh lớp 10 ngu ngơ, mình đã được tiếp xúc với máy tính. Người đã thay đổi cuộc đời mình theo chiều hướng tích cực hơn. Biến mình từ thằng mọt sách không biết sử dụng máy tính trở thành một lập trình viên như hôm nay. Được tự hào kể lại câu chuyện và tự hào vì là một thành viên chân thành tại AnonyViet. Mình thực sự rất hạnh phúc khi học tập và làm việc tại đây.";
+	$scope.contentStory1 = "";
 	$scope.contentStory2 = "Trong suốt hai năm qua, tôi cảm thấy mình chưa có tiến bộ nhiều trong công việc lẫn học tập. Cảm thấy mình cần bổ sung nhiều kiến thức hơn, có trách nhiệm hơn trong công việc và lời hứa. Gần đây, website đã bước sang giai đoạn phát triển, member trông đợi khá nhiều từ tôi và các Admin khác. Cho nên tôi sẽ cố gắng nhiều hơn nữa để mọi người không thất vọng. Xin cảm ơn mọi người !";
 	$scope.storytitle1 = "Đời lập trình của tôi bắt đầu bằng sự tình cờ ...";
 	$scope.storytitle2 = "Lời hứa của tôi với bản thân và mọi người trong hai năm qua ...";
